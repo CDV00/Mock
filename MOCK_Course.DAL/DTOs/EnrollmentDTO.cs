b303d3bee44b504d14c317a724432553884d8bad
@caodinhvu00 <caodinhvu00@gmail.com>
2022-06-13 11:44:42 +0700

    commit api GetAllCourseReviewOfIntructor

--- MOCK_Course.DAL/DTOs/EnrollmentDTO.cs
+++ MOCK_Course.DAL/DTOs/EnrollmentDTO.cs
@@ -8,6 +8,6 @@ namespace Course.BLL.Responses
     {
         public Guid Id { get; set; }
         public UserDTO User { get; set; }
-        public CourseDTO Course { get; set; }
+        public CourseDTO Courses { get; set; }
     }
 }
