77dcc40c7eec108fb445b600c99c982ea10928c4
tungnh230802 <tungnh230802@gmail.com>
2022-06-15 11:02:12 +0700

    merge qui

--- MOCK_Course.DAL/DTOs/EnrollmentDTO.cs
+++ MOCK_Course.DAL/DTOs/EnrollmentDTO.cs
@@ -1,9 +1,11 @@
 ﻿using Course.BLL.DTO;
+using System;
 
 namespace Course.BLL.Responses
 {
     public class EnrollmentDTO
     {
+        public Guid Id { get; set; }
         public UserDTO User { get; set; }
         public CourseDTO Courses { get; set; }
     }
